584332a3194fd375a194d80ca11faa0701ed6937
Ashikul Islam <ashikulislam780@gmail.com>
2023-01-21 01:30:17 +0600

    Login & SignUp UI Updated

---
+++ src/Main/Controller.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ashik
+ */
+public class Controller {
+    
+    public void Login(ActionEvent event) throws IOException
+    {
+     
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+  
+    public void SignUp(ActionEvent event) throws IOException
+    {
+     
+        Parent root = FXMLLoader.load(getClass().getResource("Signup.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+}
