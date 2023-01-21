b5abc3da599ea79c101ce2b1d46c696931721bcc
Ashikul Islam <ashikulislam780@gmail.com>
2023-01-21 10:41:32 +0600

    Updated Ui

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -16,9 +16,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         
         
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         Scene scene = new Scene(root); 
-        primaryStage.setTitle("Todo App");
+        primaryStage.setTitle("Booking Management System");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
