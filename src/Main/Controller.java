bdcd543af92251f77349585745ee57c80f019864
Ashikul Islam <ashikulislam780@gmail.com>
2023-01-21 23:32:35 +0600

    Dashboard Deleted

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -39,4 +39,14 @@ public class Controller {
         stage.show();
         
     }
+    public void Dashboard(ActionEvent event) throws IOException
+    {
+     
+        Parent root = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
 }
