90999f9acf11f616a8a0e3524c3e6a15fd391c6a
Ashikul Islam <ashikulislam780@gmail.com>
2023-01-21 19:25:45 +0600

    App Icon Changed

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,6 +19,8 @@ public class Main extends Application {
         
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         Scene scene = new Scene(root); 
+        Image image = new Image("/icons/integrated-system.png");
+        primaryStage.getIcons().add(image);
         primaryStage.setTitle("Booking Management System");
         primaryStage.setScene(scene);
         primaryStage.show();
