ccc50237efb7a48656bf41c5910d61ab4175bce6
Ashikul Islam <ashikulislam780@gmail.com>
2023-01-26 11:59:22 +0600

    UI Added

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
@@ -18,45 +19,58 @@ import javafx.stage.Stage;
  * @author ashik
  */
 public class Controller {
-    
-    public void Login(ActionEvent event) throws IOException
-    {
-     
+
+    public void Login(ActionEvent event) throws IOException {
+
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-  
-    public void SignUp(ActionEvent event) throws IOException
-    {
-     
+
+    public void SignUp(ActionEvent event) throws IOException {
+
         Parent root = FXMLLoader.load(getClass().getResource("Signup.fxml"));
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        
+
     }
-    public void Dashboard(ActionEvent event) throws IOException
-    {
-     
+
+    public void Dashboard(ActionEvent event) throws IOException {
+
         Parent root = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        
+
     }
-    public void SignOut(ActionEvent event) throws IOException
-    {
-     
+
+    public void SignOut(ActionEvent event) throws IOException {
+
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        
+
+    }
+    @FXML
+    private AnchorPane addmovie;
+
+    @FXML
+    private AnchorPane dashboard;
+
+    public void addmovie() {
+        addmovie.setVisible(true);
+        dashboard.setVisible(false);
+    }
+    public void dashboard()
+    {
+        addmovie.setVisible(false);
+        dashboard.setVisible(true);
     }
 }
